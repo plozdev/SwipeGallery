a6546e0da6b2f8fe7697f01400525e238f41405a
plozdev <hoangmai.it.dev@gmail.com>
2026-05-07 10:09:46 +0700

    first commit

---
+++ composeApp/src/commonMain/kotlin/plozdev/swipegallery/Greeting.kt
@@ -0,0 +1,9 @@
+package plozdev.swipegallery
+
+class Greeting {
+    private val platform = getPlatform()
+
+    fun greet(): String {
+        return "Hello, ${platform.name}!"
+    }
+}
---
+++ composeApp/src/iosMain/kotlin/plozdev/swipegallery/MainViewController.kt
@@ -0,0 +1,5 @@
+package plozdev.swipegallery
+
+import androidx.compose.ui.window.ComposeUIViewController
+
+fun MainViewController() = ComposeUIViewController { App() }
