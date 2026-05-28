d345dab5410ce2e604d9c0879aed9b932d18b8ec
plozdev <hoangmai.it.dev@gmail.com>
2026-05-28 13:15:49 +0700

    feat (images): add request permission for iOS

---
+++ composeApp/src/iosMain/kotlin/plozdev/swipegallery/data/IosMediaPermissionManager.kt
@@ -0,0 +1,28 @@
+package plozdev.swipegallery.data
+
+import kotlinx.coroutines.suspendCancellableCoroutine
+import platform.Photos.PHAuthorizationStatusAuthorized
+import platform.Photos.PHAuthorizationStatusLimited
+import platform.Photos.PHPhotoLibrary
+import plozdev.swipegallery.data.media.MediaPermissionManagerI
+import kotlin.coroutines.resume
+
+/**
+ * In ios, check and request permission not tied to UI Lifecycle
+ * so just use org PHPPhotoLibrary
+ */
+class IosMediaPermissionManager : MediaPermissionManagerI {
+    override suspend fun requestPermissions(): Boolean {
+        val status = PHPhotoLibrary.authorizationStatus()
+        // return true if it has permission
+        // if still not have permission yet, shows pop-up for permission
+        return if (status == PHAuthorizationStatusAuthorized || status == PHAuthorizationStatusLimited) true
+            else suspendCancellableCoroutine { cont ->
+            PHPhotoLibrary.requestAuthorization { newStatus ->
+                val isGranted = newStatus == PHAuthorizationStatusAuthorized || newStatus == PHAuthorizationStatusLimited
+                cont.resume(isGranted)
+            }
+        }
+
+    }
+}
